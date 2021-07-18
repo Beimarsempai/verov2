7f225c8c46dfadb4a959a3506d944a6503648220
Beimarsempai <87555802+Beimarsempai@users.noreply.github.com>
2021-07-18 17:01:54 -0400

    Add files via upload

---
+++ src/shantera.js
@@ -0,0 +1,75 @@
+const shantera = (prefix, pushname) => {
+    return `*Palabras especificas para que el bot interactue con ustedes*
+
+
+    Este bot no sirve
+    bañate
+    pack
+    gime
+    Quien es tu sempai?
+    yamete
+    buenos dias
+    Aa
+    uwu
+    te amo
+    konede
+    kurumi
+    vemos Netflix?
+    Gimeme
+    onichan
+    senpai
+    Ha!
+    Goku s
+    Soy gracioso?
+    Diego
+    te importo?
+    No sirves Vero
+    sate sate
+    soy adicto?
+    ¡A
+    Vero no sirves
+    ahder
+    gay
+    vete Vero
+    me gustan los trap
+    roar
+    un negro
+    chistoso
+    que?
+    puedo jugar papu?
+    pero
+    Chau Vero
+    Holi Vero
+    canta Vero
+    Me quieres?
+    El Pepe
+    me conoces Goku?
+    hace frio
+    Di mucho texto XD
+    Que podemos jugar?
+    quieres otro platano?
+    leve el pot..*
+    C corre*
+    Que vez en mi futuro?
+    wenas
+    una salchicha?
+    gao gao
+    dime una groseria
+    i love you
+    el admin
+    me mide 5cm
+    me puedes cantar?
+    Andate
+
+    *Musica favorita de mi creador*
+    Beimar
+
+
+
+_*Ojito escribe tal y como esta en el mensaje*_
+
+ву Beimar`
+
+}
+
+exports.shantera = shantera
